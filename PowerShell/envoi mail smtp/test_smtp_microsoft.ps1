3f0d303631705a191560c412d3b1b41919f1be24
m <59730942+maelinux@users.noreply.github.com>
2024-09-02 09:59:09 +0200

    Update test_smtp_microsoft.ps1

--- PowerShell/envoi mail smtp/test_smtp_microsoft.ps1
+++ PowerShell/envoi mail smtp/test_smtp_microsoft.ps1
@@ -1,7 +1,7 @@
 $email = Read-Host "saisir adresse mail source"
 Write-Host -ForegroundColor "red" "saisir les identifiants (adresse mail + mdp application)"
 $identifiants = Get-Credential
-Send-MailMessage -From $email -To "supervision@connecs-informatique.fr" -Subject "Test Email" -Body "Test SMTP authentifie microsoft 365" -SmtpServer smtp.office365.com -Credential $identifiants -UseSsl -Port 587 
+Send-MailMessage -From $email -To "SAISIR@VOTREADRESSE.MAIL" -Subject "Test Email" -Body "Test SMTP authentifie microsoft 365" -SmtpServer smtp.office365.com -Credential $identifiants -UseSsl -Port 587 
 
 $saisie = Read-Host "Refaire un essai d'envoi de mail ? (y/n)"
 while ("y","n" -notcontains $saisie) {
